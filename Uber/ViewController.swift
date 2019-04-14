d6f9b86f25fa1331d41c949d94e626665b865d9a
Ricardo Hui <eehaha@gmail.com>
2019-04-15 07:54:07 +0800

    Add Firebase Login and SignUp

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -7,14 +7,103 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var riderDriverSwitch: UISwitch!
+    
+    @IBOutlet var topButton: UIButton!
+    
+    @IBOutlet var bottomButton: UIButton!
+    
+    @IBOutlet var riderLabel: UILabel!
+    
+    @IBOutlet var driverLabel: UILabel!
+    
+    var signUpMode = true
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextField.text == "" || passwordTextField.text == ""{
+            displayAlert(title: "Missing Credential", message: "You must provide both email and password.")
+        }else{
+            
+            if let email = emailTextField.text{
+                if let password = passwordTextField.text{
+                    if signUpMode {
+                        //SIGN UP
+                        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            }else{
+                                print("Sign Up Success")
+                            }
+                        }
+                        
+                        
+                    }else{
+                        //LOG IN
+                        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                            if error != nil{
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            }else{
+                                print("Log In Success")
+                            }
+                        }
+                        
+                        
+                    }
+                }
+            }
+            
+            
+            
+        }
+        
+    }
+    
+    func displayAlert(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode{
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        }else{
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+        
+        
+    }
+    
+    
+    
+    
 }
 
